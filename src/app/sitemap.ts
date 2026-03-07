6e70bf3ff1a925a688639eadefda3b1dcefe8ad4
souvik samanta <work03.souvik@gmail.com>
2026-03-07 10:14:49 +0530

    Update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,28 +3,28 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://magiccode-dev.vercel.app",
+      url: "https://magiccode-dev.vercel.app/",
       lastModified: new Date(),
     },
     {
-      url: "https://magiccode-dev.vercel.app/learn",
+      url: "https://magiccode-dev.vercel.app/learn/",
       lastModified: new Date(),
     },
     {
-      url: "https://magiccode-dev.vercel.app/problems",
+      url: "https://magiccode-dev.vercel.app/problems/",
       lastModified: new Date(),
     },
     {
-      url: "https://magiccode-dev.vercel.app/leaderboard",
+      url: "https://magiccode-dev.vercel.app/leaderboard/",
       lastModified: new Date(),
     },
     {
-      url: "https://magiccode-dev.vercel.app/login",
+      url: "https://magiccode-dev.vercel.app/login/",
       lastModified: new Date(),
     },
     {
-      url: "https://magiccode-dev.vercel.app/signup",
+      url: "https://magiccode-dev.vercel.app/signup/",
       lastModified: new Date(),
-    }
+    },
   ];
 }
