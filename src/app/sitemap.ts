05246ef3e98bc28b40abe8d9388349e059000de7
souvik samanta <work03.souvik@gmail.com>
2026-03-07 09:54:57 +0530

    Create

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://magiccode-dev.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://magiccode-dev.vercel.app/learn",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://magiccode-dev.vercel.app/problems",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://magiccode-dev.vercel.app/leaderboard",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://magiccode-dev.vercel.app/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://magiccode-dev.vercel.app/signup",
+      lastModified: new Date(),
+    }
+  ];
+}
